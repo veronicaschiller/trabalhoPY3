edc639789bf2652b45318c51a6e8c9b960c37886
veronicaschiller <veronicaschiller17@gmail.com>
2023-06-22 12:46:41 -0300

    em construção

--- trabalho3.py
+++ trabalho3.py
@@ -2,20 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 # urls
 url_vanguarda = "https://www.livrariavanguarda.com.br/"
-url_saraiva = "https://www.saraiva.com.br/"
+url_submarino = "https://www.submarino.com.br/"
 # get das urls
 vanguarda = requests.get(url_vanguarda)
-saraiva = requests.get(url_saraiva)
+submarino = requests.get(url_submarino)
 # get do html
 html_vanguarda = BeautifulSoup(vanguarda.content, "html.parser")
-html_saraiva = BeautifulSoup(saraiva.content, "html.parser")
-# formatação
-tamplate_vanguarda = html_vanguarda.find("main", class_="main-content")
-tamplate_saraiva = html_saraiva.find(
-    "div", class_="flex flex-grow-1 w-100 flex-column")
-
-card_livros_vanguarda = tamplate_vanguarda.find_all(
-    "div", attrs={"slick-track"})
+html_submarino = BeautifulSoup(submarino.content, "html.parser")
 
 # funçoes
 def titulo(txt , traco="-"):
@@ -26,9 +19,25 @@ def titulo(txt , traco="-"):
 
 def ofertas():
     titulo("busca ofertas - melhor valor ")
+    # pesq  valor 
     nome_livro = input("Qual livro deseja ver o melhor preço? ");
-    url_vanguarda = f"https://www.livrariavanguarda.com.br/busca/" +nome_livro
-    url_saraiva = f"https://www.saraiva.com.br/{nome_livro}?"
+# loop
+    for livro in nome_livro:
+        livro = livro.strip()
+        url_vanguarda = f"https://www.livrariavanguarda.com.br/busca/" + nome_livro
+        url_submarino = f"https://www.submarino.com.br/busca/" + nome_livro
+        # vanguarda resposta
+        loja_vanguarda= requests.get(url_vanguarda)
+        busca_vanguarda = loja_vanguarda.content
+        soup_vanguarda = BeautifulSoup(busca_vanguarda,"html.parser")
+        reusltado_vanguarda = soup_vanguarda.find("div", class_="product-item")
+        # submarino resposta
+        loja_submarino= requests.get(url_submarino)
+        busca_submarino = loja_submarino.content
+        soup_submarino = BeautifulSoup(busca_submarino,"html.parser")
+        reusltado_submarino = soup_submarino.find("div", class_="vtex-slider-layout")
+
+        print(reusltado_submarino)
 
     armazena_valore =[]
 
@@ -53,14 +62,15 @@ def pesq():
 
 while True:
     print("-"*40)
-    print("ofertas                                                   ")
-    print("ranking de preços do box de hp           ")
-    print("ranking de preços do box de brigertom")
-    print("estatistica de melhor preço de livro       ")
-    print("pesquisa                                                 ")
-    print("Finalizar                                                  ")
+    print("1 ofertas                                                   ")
+    print("2 ranking de preços do box de hp                            ")
+    print("3 ranking de preços do box de brigertom                     ")
+    print("4 estatistica de melhor preço de livro                      ")
+    print("5 pesquisa                                                  ")
+    print("6 Finalizar                                                 ")
     print("-"*40)
-    opcao = int(input("Opção: "))
+
+    opcao = int(input('Opção: '))
 
     match opcao:
         case 1:
